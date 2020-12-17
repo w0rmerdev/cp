9d91514ab0e321b9ad0ffb81ad4338df9cfa06f5
Tomás Antunes <tomasbantunes@gmail.com>
2020-12-17 14:51:05 +0000

    c++

---
+++ HackerRank_c++_learn/cpp_base_data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+	int i;
+	long l;
+	char c;
+	float f;
+	double d;
+
+	cin >> i >> l >> c >> f >> d;
+
+	printf("%d \n", i);
+	printf("%ld \n", l);
+	printf("%c \n", c);
+	printf("%.3lf \n", f);
+	printf("%lf", d);
+
+	return 0;
+
+}
---
+++ HackerRank_c++_learn/cpp_for_loops1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+	
+	cin >> a;
+//	cin >> b;
+/*
+	for(int n = a; n <= b; n++){
+
+		if(n >= 1 && n <= 9){
+			if(n == 1){
+				cout << "one";
+			} else if(n == 2){
+				cout << "two";
+			} else if(n==3){
+				cout << "third";
+			} else if(n==4){
+				cout << "four";
+			} else if(n==5){
+				cout << "five";
+			} else if(n==6){
+				cout << "six";
+			} else if(n==7){
+				cout << "seven"; 
+			} else if(n==8){
+				cout << "eight";
+			} else if(n==9){
+				cout << "nine";
+			} else {
+				return;
+			}
+		} else if(n>9 && n%2==0){
+			cout << "even";
+		} else if(n>9 && n%2!=0){
+			cout << "odd";
+		}
+
+	}
+*/
+	
+	cout << a << endl;
+
+	return 0;
+
+}
